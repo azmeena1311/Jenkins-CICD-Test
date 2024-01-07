44dd9dffd75927d98f2a91676531be841e733664
entbappy <entbappy73@gmail.com>
2024-01-07 11:29:36 +0600

    initial

---
+++ scripts/ec2_setup.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+sudo apt update 
+
+sudo apt-get update 
+
+sudo apt upgrade -y
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker $USER
+
+newgrp docker
+
+sudo apt install awscli -y
